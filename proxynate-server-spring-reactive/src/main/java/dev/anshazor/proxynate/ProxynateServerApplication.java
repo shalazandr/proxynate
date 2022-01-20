90ad160598dbb253faf0c7c66a79c887e473b9d8
Andrii Shalankevych <shalankevych@gmail.com>
2022-01-21 00:32:01 +0200

    Init

---
+++ proxynate-server-spring-reactive/src/main/java/dev/anshazor/proxynate/ProxynateServerApplication.java
@@ -0,0 +1,13 @@
+package dev.anshazor.proxynate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProxynateServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProxynateServerApplication.class, args);
+	}
+
+}
---
+++ proxynate-server-spring-reactive/src/test/java/dev/anshazor/proxynate/ProxynateServerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.anshazor.proxynate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProxynateServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
